ef0b03eb3ae0f04ae6839f83b97fe15de5ffdf64
areesha-22 <110978549+areesha-22@users.noreply.github.com>
2024-03-21 02:18:47 +0500

    Add files via upload

---
+++ if.js
@@ -0,0 +1,13 @@
+var marks = 90;
+if (marks > 90) {
+    console.log("A1");
+}
+else if (marks > 80) {
+    console.log("A");
+}
+else if (marks > 70) {
+    console.log("B");
+}
+else {
+    console.log("fail");
+}
